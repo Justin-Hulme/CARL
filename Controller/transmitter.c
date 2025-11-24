57449564b9f90748464d94e7358c54a30e77b901
Justin Hulme <justinhulme2000@gmail.com>
2025-11-24 09:02:24 -0700

    got transmitter working

--- Controller/transmitter.c
+++ Controller/transmitter.c
@@ -2,51 +2,53 @@
 #include <stdbool.h>
 
 void transmitter_init(){
-    // set A0 up for alternate function
+    // set A1 up for alternate function
     
     // enable the clock on port A
     RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
 
-    // set the mode to alternate function
-    GPIOA->MODER &= ~(0b11 << (5*2));
-    GPIOA->MODER |= 0b10 << (5*2);
+    // set the mode of PA1 to alternate function
+    GPIOA->MODER &= ~(0b11 << (1*2));
+    GPIOA->MODER |= 0b10 << (1*2);
 
-    // set the alternate function to AF1
-    GPIOA->AFR[0] &= ~(0b1111 << (5*4));
-    GPIOA->AFR[0] |= 0b0001 << (5*4);
+    // set the alternate function to AF2 (TIM5_CH2)
+    GPIOA->AFR[0] &= ~(0b1111 << (1*4));
+    GPIOA->AFR[0] |= 0b0010 << (1*4);  // AF2 for TIM5_CH2 on PA1
 
-    GPIOA->OTYPER &= ~(1 << (5*2));       // push-pull
-    GPIOA->PUPDR &= ~(0b11 << (5*2)); // no pull-up/pull-down
+    // set up as push pull and no pull-up/down
+    GPIOA->OTYPER &= ~(1 << (1));
+    GPIOA->PUPDR &= ~(0b11 << (1*2));
 
     // set the speed to very high
-    GPIOA->OSPEEDR |= 0b11 << (5*2);
+    GPIOA->OSPEEDR |= 0b11 << (1*2);
 
     // set up the timer
 
-    // enable the clock
-    RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
+    // enable the clock for TIM5
+    RCC->APB1ENR1 |= RCC_APB1ENR1_TIM5EN;
 
     // set direction to up counting
-    TIM2->CR1 &= ~TIM_CR1_DIR;
+    TIM5->CR1 &= ~TIM_CR1_DIR;
 
     // set the prescaler
-    TIM2->PSC = 15; // 16MHz/(1+15) = 1MHz = 1 us
+    TIM5->PSC = 15; // 16MHz/(1+15) = 1MHz = 1 us
 
     // set the auto-reload
-    TIM2->ARR = ARR_VAL;
+    TIM5->ARR = ARR_VAL;
 
-    // clear output compare mode
-    TIM2->CCMR1 &= ~TIM_CCMR1_OC1M;
+    // set output compare mode of channel 2 to PWM mode 1 (OC2M = 110)
+    TIM5->CCMR1 &= ~TIM_CCMR1_OC2M;
+    TIM5->CCMR1 |= (0b110 << 12);
 
-    // set to PWM mode
-    TIM2->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
+    // enable output to channel 2
+    TIM5->CCER |= TIM_CCER_CC2E;
 
-    // enable output to channel 1
-    TIM2->CCER |= TIM_CCER_CC1E;
+    // enable update interrupt
+    TIM5->DIER |= TIM_DIER_UIE;
 
-    // enable interrupt
-    TIM2->DIER |= TIM_DIER_UIE;
-    NVIC_EnableIRQ(TIM2_IRQn);
+    // enable TIM5 IRQ in NVIC
+    NVIC_SetPriority(TIM5_IRQn, 2);
+    NVIC_EnableIRQ(TIM5_IRQn);
 }
 
 uint8_t num_left_to_send = 0;
@@ -58,54 +60,45 @@ void send_data(uint8_t data, uint8_t tag){
     while (!done_sending);
 
     // set up the data
-    num_left_to_send = 10;
-    to_send = (uint16_t)tag << 8 | data;
+    num_left_to_send = BITS_PER_PACKET;
+    to_send = ((uint16_t)tag << 8) | data;
 
     // reset counter
-    TIM2->CNT = 0;
-
-    // // preload first bit
-    // if (to_send & (1 << (num_left_to_send - 1))) {
-    //     TIM2->CCR1 = ONE_VAL;
-    // } else {
-    //     TIM2->CCR1 = ZERO_VAL;
-    // }
-    // num_left_to_send--;
+    TIM5->CNT = 0;
 
     // force shadow register update NOW
-    TIM2->EGR = TIM_EGR_UG;
+    TIM5->EGR = TIM_EGR_UG;
 
     done_sending = false;
 
     // start timer
-    TIM2->CR1 |= TIM_CR1_CEN;
+    TIM5->CR1 |= TIM_CR1_CEN;
 }
 
-// fires in ARR
-void TIM2_IRQHandler(){
-    if (TIM2->SR & TIM_SR_UIF) {
-        TIM2->SR &= ~TIM_SR_UIF; // clear flag
+// fires in update interrupt (when counter overflows)
+void TIM5_IRQHandler(){
+    if (TIM5->SR & TIM_SR_UIF) {
+        TIM5->SR &= ~TIM_SR_UIF; // clear update interrupt flag
         
         // if we have sent everything
         if (num_left_to_send == 0){
             // disable timer
-            TIM2->CR1 &= ~TIM_CR1_CEN;
+            TIM5->CR1 &= ~TIM_CR1_CEN;
 
             // set output low
-            TIM2->CCR1 = 0;
+            TIM5->CCR2 = 0;
 
             done_sending = true;
             return;
         }
 
         // send next bit
-        if ((to_send & (0b1 << (num_left_to_send - 1))) == 0){
-            TIM2->CCR1 = ZERO_VAL;
+        if ((to_send & (1 << (num_left_to_send - 1))) == 0){
+            TIM5->CCR2 = ZERO_VAL;
         }
         else {
-            TIM2->CCR1 = ONE_VAL;
+            TIM5->CCR2 = ONE_VAL;
         }
-        num_left_to_send --;
-
+        num_left_to_send--;
     }
 }
--- Controller/transmitter.h
+++ Controller/transmitter.h
@@ -6,6 +6,8 @@
 #define ARR_VAL 800
 #define ZERO_VAL 200
 #define ONE_VAL 450
+#define PACKET_GAP_DURATION 2400
+#define BITS_PER_PACKET 10
 
 void transmitter_init();
 void send_data(uint8_t data, uint8_t tag);
