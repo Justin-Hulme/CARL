0d8e29618fa0e81c11b31731783a91cfd4cd3bef
tracetaylor31 <tracetaylor1031@gmail.com>
2025-12-02 10:31:24 -0700

    accelerometer files

--- Controller/main.c
+++ Controller/main.c
@@ -1,28 +1,43 @@
 #include "stm32l476xx.h"
-
+#include "accelerometer.h"
 #include "uart.h"
-#include "transmitter.h"
-
 #include "stdio.h"
+#include "string.h" // Added to use strlen()
 
-int main(){
-	// select HSI as main clock
+// Function to configure System Clock to run on HSI (16 MHz)
+void System_Clock_Init(void) {
 	RCC->CR |= 0b1 << 8;
 	while ((RCC->CR & 0b1 << 10) == 0);
 	RCC->CFGR |= 1;
+}
+
+int main(void){
+    // 1. Initialize System Clock to HSI (16 MHz) first
+    System_Clock_Init();
+
+    // 2. Initialize UART 
+    // uart.c hardcodes BRR to 0x683, which assumes 16MHz. 
 
 	initialize_uart2();
+    
+    // 3. Initialize Accelerometer (Configures I2C internally)
+    Accelerometer_Init();
+    
+    int16_t x, y, z;
+    char buffer[50];
 
-	transmitter_init();
-	
-	while (1){
-		int number = uart_read_number(USART2);
-		
-		uint8_t data = number & 0b11111111;
-		uint8_t tag = number >> 8;
-
-    	send_data(data, tag);
-	}
-	
-	while(1);
+    while (1){
+        // Read values
+        Accelerometer_Read_Values(&x, &y, &z);
+        
+        // Format string
+        snprintf(buffer, 50, "X: %d, Y: %d, Z: %d\r\n", x, y, z);
+        
+        // Send to UART using the new API
+        // uart_send takes the USART instance, the buffer cast to uint8_t*, and the length
+        uart_send(USART2, (uint8_t*)buffer, strlen(buffer));
+        
+        // Simple delay loop 
+        for(int i = 0; i < 20000; i++);
+    }
 }
